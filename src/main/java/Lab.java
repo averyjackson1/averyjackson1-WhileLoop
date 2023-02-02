65bce3a67f3a89b3a8ca3113245f5a0d098fc849
averyjackson1 <jackson.avery11@gmail.com>
2023-02-02 01:17:08 +0000

    Done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -35,9 +35,19 @@ public class Lab {
     public int whileExample(int end){
         int sum = 0;
         int i = 0;
+        
 
-//        code here
-
+        while(i < end){
+        
+           sum = sum + i;
+           i = i + 1;
+           
+        
+        
+        System.out.println(sum);
+     
+        
+        }
         return sum;
     }
 }
